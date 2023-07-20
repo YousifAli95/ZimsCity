2641804f8dbbb0963adee42b99cfc68aab182281
Yousif Ali <Yousifali95@hotmail.co.uk>
2023-07-20 14:26:58 +0200

    Added UI design for login page and signup page

--- YousifsSolution/YousifsProject/Views/Identity/LoginVM.cs
+++ YousifsSolution/YousifsProject/Views/Identity/LoginVM.cs
@@ -4,16 +4,16 @@ namespace YousifsProject.Views.Identity
 {
     public class LoginVM
     {
-        [StringLength(maximumLength: 100)]
+        [StringLength(maximumLength: 100, MinimumLength = 6, ErrorMessage = "Your Username must be between 6 and 100 characters long.")]
         [Display(Name = "Username")]
         [Required]
-        public string? Username { get; set; }
+        public string Username { get; set; }
 
-        [StringLength(maximumLength: 100)]
+        [StringLength(maximumLength: 100, MinimumLength = 6, ErrorMessage = "Your Password must be between 6 and 100 characters long.")]
         [DataType(DataType.Password)]
         [Required]
         [Display(Name = "Password")]
-        public string? Password { get; set; }
+        public string Password { get; set; }
     }
 
 }
--- YousifsSolution/YousifsProject/Views/Identity/SignupVM.cs
+++ YousifsSolution/YousifsProject/Views/Identity/SignupVM.cs
@@ -4,22 +4,22 @@ namespace YousifsProject.Views.Identity
 {
     public class SignupVM
     {
-        [StringLength(maximumLength: 100)]
+        [StringLength(maximumLength: 100, MinimumLength = 6, ErrorMessage = "Your Username must be between 6 and 100 characters long.")]
         [Display(Name = "Username")]
         [Required]
-        public string? Username { get; set; }
+        public string Username { get; set; }
 
-        [StringLength(maximumLength: 100)]
+        [StringLength(maximumLength: 100, MinimumLength = 6, ErrorMessage = "Your Password must be between 6 and 100 characters long.")]
         [DataType(DataType.Password)]
         [Required]
         [Display(Name = "Password")]
-        public string? Password { get; set; }
+        public string Password { get; set; }
 
-        [StringLength(maximumLength: 100)]
+        [StringLength(maximumLength: 100, MinimumLength = 6, ErrorMessage = "Your Confirm Password must be between 6 and 100 characters long.")]
         [DataType(DataType.Password)]
         [Required]
         [Display(Name = "Confirm Password")]
         [Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
-        public string? ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
