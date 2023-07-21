f1f0b42235d0cb386772a45ee5fa3bdc130247e1
Yousif Ali <Yousifali95@hotmail.co.uk>
2023-07-21 09:36:10 +0200

    Fix destroy all houses modal

--- YousifsSolution/YousifsProject/wwwroot/JS/Navbar.js
+++ YousifsSolution/YousifsProject/wwwroot/JS/Navbar.js
@@ -1,9 +1,19 @@
-﻿function destroy() {
-    const modalBackground = document.querySelector(".Modal-back");
+﻿function showModal() {
+    const modalBackground = document.querySelector(".modal-back");
     modalBackground.style.display = "block" 
     }
 
-function noDestruction() {
-    const modalBackground = document.querySelector(".Modal-back");
+function hideModal() {
+    const modalBackground = document.querySelector(".modal-back");
     modalBackground.style.display = "none"
 }
+
+document.querySelector('.modal-back').addEventListener('click', function (event) {
+    var modalBack = document.querySelector('.modal-back');
+    var modal = document.querySelector('.modal');
+
+    // If the event target is not the modal, but the background, close modal
+    if (event.target !== modal && event.target === modalBack) {
+        hideModal();
+    }
+});
