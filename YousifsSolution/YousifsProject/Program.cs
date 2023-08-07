09653fe6c0e3e3edae6bfe2d71477b4af5f091b6
Yousif Ali <Yousifali95@hotmail.co.uk>
2023-08-07 15:50:31 +0200

    Remove default logging providers

--- YousifsSolution/YousifsProject/Program.cs
+++ YousifsSolution/YousifsProject/Program.cs
@@ -14,6 +14,7 @@ namespace YousifsProject
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Logging.ClearProviders();
 
             builder.Services.AddScoped<CustomLogger>();
             builder.Services.AddControllersWithViews();
